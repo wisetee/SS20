9e7a9b5f14a34e9a44211188e6f40ca36bf0c62c
Tepkimsour <Tep-kimsour@outlook.com>
2023-08-17 19:15:51 +0700

    tepkimsour

--- tepkimsour.php
+++ tepkimsour.php
@@ -1,3 +1,17 @@
-<?php
-    echo 'tep kimsour ss20';
-?>
+<?php 
+    if(isset($_POST["buttonSubmit"])){ 
+ 
+        $source = $_FILES["fileUploading"]["tmp_name"]; 
+ 
+        $name = $_FILES["fileUploading"]["name"]; 
+        $name = time() . md5(rand()) . "." . end(explode(".", $name)); 
+        $destination = "media/" . $name; 
+         
+        move_uploaded_file($source, $destination); 
+         
+    } 
+?> 
+<form action="" method="post" enctype="multipart/form-data"> 
+    <input type="file" name="fileUploading" id="fileUploading"> 
+    <input type="submit" value="UPLOAD" name="buttonSubmit"> 
+</form>
